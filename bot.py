0e8b31013f864b40ed5bba79c03069e180470e76
prikl11 <119262114+prikl11@users.noreply.github.com>
2025-08-30 16:00:39 +0300

    Add /tomorrow and /nextweek functions

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from telegram.constants import ParseMode
-from schedule_api import get_schedule, get_today_schedule
+from schedule_api import get_schedule, get_today_schedule, get_tomorrow_schedule, get_next_week_schedule
 import os
 from dotenv import load_dotenv
 
@@ -13,8 +13,11 @@ DEFAULT_GROUP = "СВБО-01-23"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Привет! Это бот расписания.\n\n"
-        "Используйте команду /today, чтобы узнать сегодняшнее расписание,"
-        " либо /week для расписания на неделю.\n\n"
+        "Команды бота:\n"
+        "• /today — расписание на сегодня\n"
+        "• /tomorrow — расписание на завтра\n"
+        "• /week — расписание на эту неделю\n"
+        "• /nextweek — расписание на следующую неделю\n\n"
         "ℹ️ На данный момент бот поддерживает только группу СВБО-01-23."
     )
 
@@ -39,6 +42,16 @@ async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = get_today_schedule(id_client=3, id_group=group_id)
     await update.message.reply_text(text, parse_mode=ParseMode.HTML)
 
+async def tomorrow(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    group_id = GROUPS[DEFAULT_GROUP]
+    text = get_tomorrow_schedule(id_client=3, id_group=group_id)
+    await update.message.reply_text(text, parse_mode=ParseMode.HTML)
+
+async def next_week(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    group_id = GROUPS[DEFAULT_GROUP]
+    text = get_next_week_schedule(id_client=3, id_group=group_id)
+    await update.message.reply_text(text, parse_mode=ParseMode.HTML)
+
 if __name__ == "__main__":
     TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
@@ -46,5 +59,7 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("week", week))
     app.add_handler(CommandHandler("today", today))
+    app.add_handler(CommandHandler("tomorrow", tomorrow))
+    app.add_handler(CommandHandler("nextweek", next_week))
 
     app.run_polling()
