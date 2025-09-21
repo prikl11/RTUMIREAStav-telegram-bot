2544a145be3fc19dcfce20e3d60b9ab60affee69
prikl11 <119262114+prikl11@users.noreply.github.com>
2025-09-21 14:37:04 +0300

    Modified stats, add logs

--- bot.py
+++ bot.py
@@ -5,11 +5,13 @@ from schedule_api import get_schedule, get_today_schedule, get_tomorrow_schedule
 import os
 from dotenv import load_dotenv
 from group_api import get_group_id
-from db import init_db, get_user_group_db, read_users, set_user_group_db
+from db import init_db, get_user_group_db, read_users, set_user_group_db, get_users_number_by_groups, log_usage, get_command_usage_number, get_count_command_usage, get_usage
+import asyncio
 
 load_dotenv()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log_usage("/start")
     user_data = get_user_group_db(update.effective_user.id)
     if not user_data:
         await update.message.reply_text("⚠️ Сначала укажите группу: /group <название группы>")
@@ -26,6 +28,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 async def set_group(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log_usage("/group")
     if not context.args:
         await update.message.reply_text("Введите /group <название группы>")
         return
@@ -47,6 +50,7 @@ def get_user_group(user_id: int):
     return None
 
 async def week(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log_usage("/week")
     group_id = get_user_group(update.effective_user.id)
     if not group_id:
         await update.message.reply_text("⚠️ Сначала укажите группу: /group <название группы>")
@@ -56,6 +60,7 @@ async def week(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(text, parse_mode=ParseMode.HTML)
 
 async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log_usage("/today")
     group_id = get_user_group(update.effective_user.id)
     if not group_id:
         await update.message.reply_text("⚠️ Сначала укажите группу: /group <название группы>")
@@ -65,6 +70,7 @@ async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(text, parse_mode=ParseMode.HTML)
 
 async def tomorrow(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log_usage("/tomorrow")
     group_id = get_user_group(update.effective_user.id)
     if not group_id:
         await update.message.reply_text("⚠️ Сначала укажите группу: /group <название группы>")
@@ -74,6 +80,7 @@ async def tomorrow(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(text, parse_mode=ParseMode.HTML)
 
 async def next_week(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log_usage("/nextweek")
     group_id = get_user_group(update.effective_user.id)
     if not group_id:
         await update.message.reply_text("⚠️ Сначала укажите группу: /group <название группы>")
@@ -83,11 +90,36 @@ async def next_week(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(text, parse_mode=ParseMode.HTML)
 
 async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        admin_id = int(os.getenv("ADMIN_ID"))
-        if update.effective_user.id == admin_id:
-            await update.message.reply_text(f"Количество пользователей: {read_users()}")
-            return
+    admin_id = int(os.getenv("ADMIN_ID"))
+    if update.effective_user.id == admin_id:
+        message = f"Количество пользователей: {read_users()}\n"
+        users_by_groups = await asyncio.get_event_loop().run_in_executor(None, get_users_number_by_groups)
 
+        message += f"Количество пользователей по группам:\n"
+        for group, count in users_by_groups.items():
+            message += f"{group}: {count}\n"
+
+        await update.message.reply_text(message, parse_mode=ParseMode.HTML)
+        return
+
+async def commands_stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    admin_id = int(os.getenv("ADMIN_ID"))
+    if update.effective_user.id == admin_id:
+        message = f"Команд использовано: {get_count_command_usage()}\n"
+
+        message += f"Использование команд:\n"
+
+        usage = await asyncio.get_event_loop().run_in_executor(None, get_command_usage_number)
+        for command, count in usage.items():
+            message += f"{command}: {count}\n"
+
+        await update.message.reply_text(message, parse_mode=ParseMode.HTML)
+
+async def usage(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    admin_id = int(os.getenv("ADMIN_ID"))
+    if update.effective_user.id == admin_id:
+        period = context.args[0]
+        await update.message.reply_text(f"В течение данного периода использовано команд: {get_usage(period)}")
 
 if __name__ == "__main__":
     init_db()
@@ -102,5 +134,7 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("nextweek", next_week))
     app.add_handler(CommandHandler("group", set_group))
     app.add_handler(CommandHandler("stats", stats))
+    app.add_handler(CommandHandler("commands", commands_stats))
+    app.add_handler(CommandHandler("usage", usage))
 
     app.run_polling()
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 def init_db():
     conn = sqlite3.connect("users.db")
@@ -10,13 +11,69 @@ def init_db():
         group_id INTEGER
     )
     """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS bot_usage (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        command TEXT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    """)
     conn.commit()
     conn.close()
+
+def log_usage(command: str):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    INSERT INTO bot_usage (command) VALUES (?)
+    """, (command,) )
+    conn.commit()
+    conn.close()
+
+def get_count_command_usage():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM bot_usage")
+    count = cursor.fetchone()[0]
+    conn.close()
+    return count
+
+def get_command_usage_number():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT command, COUNT(*) FROM bot_usage GROUP BY command")
+    result = {command: count for command, count in cursor.fetchall()}
+    conn.close()
+    return result
 
 def read_users():
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
     cursor.execute("SELECT COUNT(*) FROM users")
+    count = cursor.fetchone()[0]
+    conn.close()
+    return count
+
+def get_users_number_by_groups():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT group_name, COUNT(*) FROM users GROUP BY group_name")
+    result = {group: count for group, count in cursor.fetchall()}
+    conn.close()
+    return result
+
+def get_usage(period: str) -> int:
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    if period == "day":
+        cursor.execute("SELECT COUNT(*) FROM bot_usage WHERE DATE(timestamp) = DATE('now')")
+    if period == "week":
+        cursor.execute("SELECT COUNT(*) FROM bot_usage WHERE DATE(timestamp) >= DATE('now', '-7 days')")
+    if period == "month":
+        cursor.execute("SELECT COUNT(*) FROM bot_usage WHERE DATE(timestamp) >= DATE('now', '-1 month')")
+
     count = cursor.fetchone()[0]
     conn.close()
     return count
